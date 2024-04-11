42da5f35d03dfef78b4eca061cfd6541b1a5282f
Mübarek ALBAYATI <mubapc1@gmail.com>
2024-04-12 02:30:52 +0300

    about page

---
+++ Comfy Store/comfy-store/src/pages/About.jsx
@@ -0,0 +1,26 @@
+const About = () => {
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem suscipit
+        obcaecati perferendis cupiditate veritatis quidem aliquid culpa
+        quibusdam libero ducimus veniam, laborum reiciendis rerum omnis
+        consequuntur. Porro, quam. Sunt, officia maxime quod architecto illo
+        porro corporis ut velit eveniet ullam.
+      </p>
+    </>
+  )
+}
+export default About
