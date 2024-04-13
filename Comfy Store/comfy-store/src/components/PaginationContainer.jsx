4f4da7345c1f44ce26366a0bbb2a74d9ed1a325b
Mübarek ALBAYATI <mubapc1@gmail.com>
2024-04-13 07:01:33 +0300

    products page

---
+++ Comfy Store/comfy-store/src/components/PaginationContainer.jsx
@@ -0,0 +1,12 @@
+import ProductsGrid from './ProductsGrid'
+import ProductsList from './ProductsList'
+
+const PaginationContainer = () => {
+  return (
+    <>
+      <ProductsList />
+      <ProductsGrid />
+    </>
+  )
+}
+export default PaginationContainer
